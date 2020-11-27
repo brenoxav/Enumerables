2c1f0075d4b7b272cfc68fb62ab14c594f285fc4
imran5644 <imi56444@gmail.com>
2020-11-28 03:12:21 +0530

    added the myeachwithindex and myselect

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -25,5 +25,23 @@ describe Enumerable do
     end
   end
 
-  
+  describe "#my_each_with_index" do
+    it "iterates through each value and index in an array" do
+      i = 0
+      arr.my_each_with_index do |e, index |
+        expect(index).to eql(i)
+        i += 1
+      end
+     end
+    end
+
+    it "should return the enumerable that\'s being to call" do
+      expect(arr.my_each_with_index {}).to eql(arr)
+    end
+
+    describe "#my_select" do
+      it "returns an array containing all elements of enum for which the given block returns a true value " do
+        expect(arr.my_select { |n| n.even? }).to eql([2, 4])
+    end
+  end
 end
